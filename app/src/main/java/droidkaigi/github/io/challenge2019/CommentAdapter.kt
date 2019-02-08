3bb67f4f5652860f5147ca09cdece84bcfaef92d
Kenji Abe <kenji.01.star@gmail.com>
2019-02-08 12:04:14 +0900

    Show ad

--- app/src/main/java/droidkaigi/github/io/challenge2019/CommentAdapter.kt
+++ app/src/main/java/droidkaigi/github/io/challenge2019/CommentAdapter.kt
@@ -4,23 +4,51 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import droidkaigi.github.io.challenge2019.data.api.response.Item
+import droidkaigi.github.io.challenge2019.databinding.ItemAdBinding
 import droidkaigi.github.io.challenge2019.databinding.ItemCommentBinding
 
 
 class CommentAdapter(
     var comments: List<Item?>
-) : RecyclerView.Adapter<CommentAdapter.ViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    class ViewHolder(val binding: ItemCommentBinding) : RecyclerView.ViewHolder(binding.root)
+    class CommentViewHolder(val binding: ItemCommentBinding) : RecyclerView.ViewHolder(binding.root)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemCommentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+    class AdViewHolder(binding: ItemAdBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position < comments.size) {
+            R.layout.item_comment
+        } else {
+            R.layout.item_ad
+        }
     }
 
-    override fun getItemCount(): Int = comments.size
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            R.layout.item_comment -> {
+                val binding = ItemCommentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                CommentViewHolder(binding)
+            }
+            R.layout.item_ad -> {
+                val binding = ItemAdBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                AdViewHolder(binding)
+            }
+            else -> throw IllegalStateException("Unknown view type")
+        }
+    }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.item = comments[position]
+    override fun getItemCount(): Int = if (comments.isNotEmpty()) {
+        comments.size + 1
+    } else {
+        0
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is CommentViewHolder -> {
+                holder.binding.item = comments[position]
+            }
+        }
     }
 }
